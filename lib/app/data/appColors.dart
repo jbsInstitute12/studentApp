8ca2eb84b35bbdef9617e0a3603c3c7c9e73850d
jbs <jbs.itinstitute@gmail.com>
2023-09-26 11:09:04 +0530

    color, icon and strings

--- lib/app/data/appColors.dart
+++ lib/app/data/appColors.dart
@@ -9,4 +9,11 @@ class AppColors {
   static Color geryColor = Colors.grey.shade200;
   static Color greyTextColor = Colors.grey.shade500;
   static Color blackTextColor = Colors.black;
+  static Color yellow = Color(0XFFFBF0B2);
+  static Color pink = Color(0XFFFFC7EA);
+  static Color purple = Color(0XFFFFC7EA);
+  static Color sky = Color(0XFFCAEDFF);
+  static Color orange  = Color(0XFFFFDBAA);
+  static Color green = Color(0XFFD0F5BE);
+
 }
