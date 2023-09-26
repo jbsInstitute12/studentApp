f7a65e719cf2b79d1932972e32ed2fea6eacea49
jbs <jbs.itinstitute@gmail.com>
2023-09-26 11:08:59 +0530

    color, string and icon

--- lib/app/data/appIcons.dart
+++ lib/app/data/appIcons.dart
@@ -2,5 +2,23 @@ import 'package:flutter/material.dart';
 
 class AppIcons {
   static IconData icCalender = Icons.calendar_today;
-  static IconData icAboutus = Icons.info;
+  static IconData icAboutus = Icons.people;
+  static IconData icManagementMessage = Icons.groups_3;
+  static IconData icPrincipalMessage = Icons.message_rounded;
+  static IconData icActivity = Icons.calendar_month;
+  static IconData icNotice = Icons.dashboard;
+  static IconData icFacility = Icons.person_4_sharp;
+  static IconData icGallery = Icons.photo_camera_back_outlined;
+  static IconData icStudentZone = Icons.book_online_rounded;
+  static IconData icAssignment = Icons.assignment_add;
+  static IconData icElibrary = Icons.library_add;
+  static IconData icEvents = Icons.event;
+  static IconData icAdmission = Icons.admin_panel_settings_rounded;
+  static IconData icAdmissionInquiry = Icons.laptop;
+  static IconData icContactUs = Icons.contact_mail_outlined;
+
+
+
+
+
 }
--- lib/app/data/appStrings.dart
+++ lib/app/data/appStrings.dart
@@ -4,6 +4,18 @@ class AppStrings {
   static const managementMessage = "Management Message";
   static const signIn = "Sing In";
   static const studentLogin = "Student Login";
+  static const principalMessage = "Principal Message";
+  static const activity = "Activity";
+  static const notice = "Notice";
+  static const facility = "Facility";
+  static const gallery = "Gallery";
+  static const studentZone = "Student Zone";
+  static const assignment = "Assignment";
+  static const admissionInquiry = "Admission Inquiry";
+  static const contactUs = "Contact Us";
+
+
+
   // static const todays="todays";
   // static const todays="todays";
   // static const todays="todays";
